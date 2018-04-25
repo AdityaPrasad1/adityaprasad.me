d7ce69cd65947c3951cd050fcf4befa94ed341f8
Aditya Prasad <aprasad101@utexas.edu>
2018-04-25 15:22:39 -0500

    Main site structure designed

--- app/main.py
+++ app/main.py
@@ -6,7 +6,19 @@ Bootstrap(app)
 
 @app.route("/")
 def index() :
-    return render_template('index.html' )
+    return render_template('index.html')
+
+@app.route("/credentials/")
+def credentials() :
+    return render_template('credentials.html')
+
+@app.route("/projects/")
+def projects() :
+    return render_template('projects.html')
+
+@app.route("/bio/")
+def credentials() :
+    return render_template('bio.html')
 
 if __name__ == "__main__" :
     app.run(host='0.0.0.0', debug=True, port=80)
